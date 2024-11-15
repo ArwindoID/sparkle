d217595efa3c472ad0ae0bba91a2a3ac549c8f1c
Arwindo <arwindoid@gmail.com>
2024-11-15 18:50:15 +0700

    feat: Implement login backend functionality

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\loginController;
-use App\Http\Controllers\registrasiController;
-use App\Http\Controllers\dashboardController;
-use App\Http\Controllers\zonaController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegistrasiController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ZonaController;
 
 // Autentikasi
-Route::get('/', [loginController::class, 'index'])->name('login');
-Route::get('/registrasi', [registrasiController::class, 'index'])->name('registrasi');
-Route::post('/logout', [loginController::class, 'index'])->name('logout');
+Route::get('/', [LoginController::class, 'index'])->name('login'); // Menampilkan halaman login
+Route::post('/login_proses', [LoginController::class, 'login_proses'])->name('login_proses'); // Memproses login (gunakan POST)
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout'); // Logout (gunakan POST)
+Route::get('/registrasi', [RegistrasiController::class, 'index'])->name('registrasi');
 
-// Dahboard
-Route::get('/dashboard', [dashboardController::class, 'index'])->name('dashboard');
+// Dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-// fitur  utama
+// Fitur Utama - Zona
 Route::prefix('zona')->name('zona.')->group(function () {
     Route::get('/1', [ZonaController::class, 'zona1'])->name('1');
     Route::get('/2', [ZonaController::class, 'zona2'])->name('2');
